2418c039b3d0bb47affd832ce85b5df12161dd16
rehanulhb <rehanulh@gmail.com>
2025-11-04 01:49:41 +0100

    Layout Component with Ant Design

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import MainLayout from "./components/layout/MainLayout"
 
 
 
@@ -6,7 +7,7 @@ function App() {
 
   return (
     <>
-      <h1>University Management Application</h1>
+      <MainLayout></MainLayout>
     </>
   )
 }
---
+++ src/components/layout/AdminLayout.tsx
@@ -0,0 +1,9 @@
+const AdminLayout = () =>{
+    return (
+        <div>
+            <h1>This is AdminLayout Component</h1>
+        </div>
+    )
+}
+
+export default AdminLayout
