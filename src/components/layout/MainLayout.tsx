1e3047011087068614928bb3cb921e8f2b4f20f2
rehanulhb <rehanulh@gmail.com>
2025-11-07 23:52:34 +0100

    A Centralised Array For Routes and Sidebar Items

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,5 +1,5 @@
 import { Layout, Menu, type MenuProps } from 'antd';
-import { Outlet } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 // import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
 // import { Children, createElement } from "react";
 
@@ -7,24 +7,24 @@ const { Header, Content, Footer, Sider } = Layout;
 
 const items: MenuProps['items'] = [
   {
-    key: '1',
-    label: 'Dashboard',
+    key: 'Dashboard',
+    label: <NavLink to="/admin/dashboard">Dashboard</NavLink>,
   },
   {
-    key: '2',
-    label: 'Profile',
-  },
-  {
-    key: '3',
+    key: 'User Management',
     label: 'User Management',
     children: [
       {
-        key: '11',
-        label: 'Create Admin',
+        key: 'Create Admin',
+        label: <NavLink to="/admin/create-admin">Create Admin</NavLink>,
       },
       {
-        key: '21',
-        label: 'Create Student',
+        key: 'Create Faculty',
+        label: <NavLink to="/admin/create-faculty">Create Faculty</NavLink>,
+      },
+      {
+        key: 'Create Student',
+        label: <NavLink to="/admin/create-student">Create Student</NavLink>,
       },
     ],
   },
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,45 +1,28 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from '../App';
-import About from '../pages/About';
-import Contact from '../pages/Contact';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
-import AdminDashboard from '../pages/admin/AdminDashboard';
-import CreateStudent from '../pages/admin/CreateStudent';
-import AdminLayout from '../components/layout/AdminLayout';
+import { adminPaths } from './admin.routes';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App></App>,
-    children: [
-      {
-        path: 'about',
-        element: <About></About>,
-      },
-      {
-        path: 'contact',
-        element: <Contact></Contact>,
-      },
-    ],
   },
   {
     path: '/admin',
-    element: <AdminLayout></AdminLayout>,
-    children: [
-      {
-        index: true,
-        element: <AdminDashboard></AdminDashboard>,
-      },
-      {
-        path: 'dashboard',
-        element: <AdminDashboard></AdminDashboard>,
-      },
-      {
-        path: 'create-student',
-        element: <CreateStudent></CreateStudent>,
-      },
-    ],
+    element: <App />,
+    children: adminPaths,
+  },
+  {
+    path: '/faculty',
+    element: <App />,
+    children: adminPaths,
+  },
+  {
+    path: '/student',
+    element: <App />,
+    children: adminPaths,
   },
   {
     path: '/login',
