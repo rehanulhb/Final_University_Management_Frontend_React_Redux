ef843df9ad90a382ff9ab9bfb694f005ca6921b1
rehanulhb <rehanulh@gmail.com>
2025-11-08 02:04:25 +0100

    Refractor Hard-coded Sidebar Items

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,34 +1,10 @@
-import { Layout, Menu, type MenuProps } from 'antd';
-import { NavLink, Outlet } from 'react-router-dom';
+import { Layout, Menu } from 'antd';
+import { Outlet } from 'react-router-dom';
+import { adminSidebarItems } from '../../routes/admin.routes';
 // import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
 // import { Children, createElement } from "react";
 
 const { Header, Content, Footer, Sider } = Layout;
-
-const items: MenuProps['items'] = [
-  {
-    key: 'Dashboard',
-    label: <NavLink to="/admin/dashboard">Dashboard</NavLink>,
-  },
-  {
-    key: 'User Management',
-    label: 'User Management',
-    children: [
-      {
-        key: 'Create Admin',
-        label: <NavLink to="/admin/create-admin">Create Admin</NavLink>,
-      },
-      {
-        key: 'Create Faculty',
-        label: <NavLink to="/admin/create-faculty">Create Faculty</NavLink>,
-      },
-      {
-        key: 'Create Student',
-        label: <NavLink to="/admin/create-student">Create Student</NavLink>,
-      },
-    ],
-  },
-];
 
 const MainLayout = () => {
   return (
@@ -53,7 +29,7 @@ const MainLayout = () => {
         >
           <h1>Uni Frontend</h1>
         </div>
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={items} />
+        <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={adminSidebarItems} />
       </Sider>
       <Layout>
         <Header style={{ padding: 0 }} />
