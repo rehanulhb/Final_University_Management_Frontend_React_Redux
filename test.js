49de6e0741dd27a1f9c3ca3778b659c1d2573554
rehanulhb <rehanulh@gmail.com>
2025-11-10 01:20:27 +0100

    Serializable And Non-Serializable Objects

--- test.js
+++ test.js
@@ -1,54 +1,54 @@
-const adminPaths2 = [
-  {
-    name: 'Dashboard',
-    path: 'dashboard',
-    element: 'ADMIN_DASHBOARD',
-  },
-  {
-    name: 'User Management',
-    children: [
-      {
-        name: 'Create Admin',
-        path: 'create-admin',
-        element: 'CREATE_ADMIN',
-      },
-      {
-        name: 'Create Faculty',
-        path: 'create-faculty',
-        element: 'CREATE_FACULTY',
-      },
-      {
-        name: 'Create Student',
-        path: 'create-student',
-        element: 'CREATE_STUDENT',
-      },
-    ],
-  },
-];
+// const adminPaths2 = [
+//   {
+//     name: 'Dashboard',
+//     path: 'dashboard',
+//     element: 'ADMIN_DASHBOARD',
+//   },
+//   {
+//     name: 'User Management',
+//     children: [
+//       {
+//         name: 'Create Admin',
+//         path: 'create-admin',
+//         element: 'CREATE_ADMIN',
+//       },
+//       {
+//         name: 'Create Faculty',
+//         path: 'create-faculty',
+//         element: 'CREATE_FACULTY',
+//       },
+//       {
+//         name: 'Create Student',
+//         path: 'create-student',
+//         element: 'CREATE_STUDENT',
+//       },
+//     ],
+//   },
+// ];
 
-const newArray = adminPaths2.reduce((acc, item) => {
-  if (item.path && item.name) {
-    acc.push({
-      key: item.name,
-      label: 'NAV_LINK',
-    });
-  }
+// const newArray = adminPaths2.reduce((acc, item) => {
+//   if (item.path && item.name) {
+//     acc.push({
+//       key: item.name,
+//       label: 'NAV_LINK',
+//     });
+//   }
 
-  if (item.children) {
-    acc.push({
-      key: item.name,
-      label: item.name,
-      children: item.children.map((child) => ({
-        key: child.name,
-        label: 'NAVLINK',
-      })),
-    });
-  }
+//   if (item.children) {
+//     acc.push({
+//       key: item.name,
+//       label: item.name,
+//       children: item.children.map((child) => ({
+//         key: child.name,
+//         label: 'NAVLINK',
+//       })),
+//     });
+//   }
 
-  return acc;
-}, []);
+//   return acc;
+// }, []);
 
-console.log(JSON.stringify(newArray));
+// console.log(JSON.stringify(newArray));
 
 // const newArray = adminPaths2.reduce((acc, item) => {
 //   if (item.path && item.element) {
