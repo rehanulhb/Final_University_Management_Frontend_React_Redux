54294655d0ff073890839ab43314f3a7d7c47542
rehanulhb <rehanulh@gmail.com>
2025-11-10 01:52:43 +0100

    Protected Route - Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import MainLayout from "./components/layout/MainLayout"
-
-
+import MainLayout from './components/layout/MainLayout';
+import ProtectedRoute from './components/layout/ProtectedRoute';
 
 function App() {
-
-
   return (
     <>
-      <MainLayout></MainLayout>
+      <ProtectedRoute>
+        <MainLayout />
+      </ProtectedRoute>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
