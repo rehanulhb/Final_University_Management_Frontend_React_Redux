0ad67b02dcd2ca0c35aed98520accc28b5da1682
rehanulhb <rehanulh@gmail.com>
2025-11-13 23:19:57 +0100

    Show Semester Data Using Ant Design Table

--- src/pages/admin/academicManagement/AcademicSemester.tsx
+++ src/pages/admin/academicManagement/AcademicSemester.tsx
@@ -1,14 +1,79 @@
+import { Table, type TableColumnsType, type TableProps } from 'antd';
 import { useGetAllSemestersQuery } from '../../../redux/features/admin/academicManagement.api';
 
-const AcademicSemester = () => {
-  const { data } = useGetAllSemestersQuery(undefined);
+interface DataType {
+  key: React.Key;
+  name: string;
+  age: number;
+  address: string;
+}
 
-  console.log(data);
+const AcademicSemester = () => {
+  const { data: semesterData } = useGetAllSemestersQuery(undefined);
+
+  const tableData = semesterData?.data?.map(({ _id, name, startMonth, endMonth, year }) => ({
+    _id,
+    name,
+    startMonth,
+    endMonth,
+    year,
+  }));
+
+  const columns: TableColumnsType<DataType> = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      showSorterTooltip: { target: 'full-header' },
+      filters: [
+        {
+          text: 'Joe',
+          value: 'Joe',
+        },
+        {
+          text: 'Jim',
+          value: 'Jim',
+        },
+        {
+          text: 'Submenu',
+          value: 'Submenu',
+          children: [
+            {
+              text: 'Green',
+              value: 'Green',
+            },
+            {
+              text: 'Black',
+              value: 'Black',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      title: 'Year',
+      dataIndex: 'year',
+    },
+    {
+      title: 'Start Month',
+      dataIndex: 'startMonth',
+    },
+    {
+      title: 'End Month',
+      dataIndex: 'endMonth',
+    },
+  ];
+
+  const onChange: TableProps<DataType>['onChange'] = (pagination, filters, sorter, extra) => {
+    console.log(filters);
+  };
 
   return (
-    <div>
-      <h1>This is AcademicSemester component</h1>
-    </div>
+    <Table<DataType>
+      columns={columns}
+      dataSource={tableData}
+      onChange={onChange}
+      showSorterTooltip={{ target: 'sorter-icon' }}
+    />
   );
 };
 
